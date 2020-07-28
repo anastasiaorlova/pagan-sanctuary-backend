bf6af3fc4df0cab4ad07866df2336c0945ae0202
residenttrickster <metkusblackus@gmail.com>
2020-07-28 15:51:24 -0400

    MVP done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,38 @@ class PostsController < ApplicationController
         render json: posts
     end 
 
+    def show 
+        post = Post.find(params[:id])
+        render json: post
+    end 
+
+    def create
+        post = Post.create(
+            title: params[:title],
+            body: params[:body],
+            user_id: 1
+        )
+        if post.valid?
+            render json: post
+        else
+        render json: { errors: post.errors.full_messages }, status: 400
+        end
+    end 
+
+    def update
+        post = Post.find(params[:id])
+        post.update(
+            title: params[:title],
+            body: params[:body])
+
+        render json: { post: post, message: "Post was edited!" }
+    end 
+
+    def destroy
+        post = Post.find(params[:id])
+        post.destroy
+
+        render json: { message: "GONE!" }
+    end 
+
 end
